de526c17724020dce6b5fc1ad7951da78142f2ed
Durjoy122 <durjoysarker520@gmail.com>
2025-11-09 14:25:37 +0600

    jwt

--- index.js
+++ index.js
@@ -2,12 +2,68 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 3000;
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./firebase-admin-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+const logger = (req, res, next) => {
+    console.log('logging information');
+    next();
+}
+
+const verifyFireBaseToken = async (req, res, next) => {
+    if(!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    try {
+        const userInfo = await admin.auth().verifyIdToken(token);
+        req.token_email = userInfo.email;
+        console.log('after token validation', userInfo);
+        next();
+    }
+    catch {
+        console.log('invalid token')
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+}
+
+const verifyJWTToken = (req, res, next) => {
+
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1];
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        // put it in the right place
+        console.log('after decoded', decoded);
+        req.token_email = decoded.email;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@myfirstmongodb.noasusn.mongodb.net/?appName=MyFirstMongoDb`;
 
@@ -26,10 +82,10 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        const db = client.db('smart_db');
-        const productsCollection = db.collection('products');
-        const bidsCollection = db.collection('bids');
-        const userCollection = db.collection('users');
+         const db = client.db('smart_db');
+         const productsCollection = db.collection('products');
+         const bidsCollection = db.collection('bids');
+         const userCollection = db.collection('users');
          app.post('/users' , async(req , res) => {
             const newUser = req.body;
             const email = req.body.email;
@@ -48,6 +104,11 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+        app.post('/getToken', (req, res) => {
+           const loggedUser = req.body;
+           const token = jwt.sign(loggedUser, process.env.JWT_SECRET, { expiresIn: '1h' })
+            res.send({ token: token })
+      })
         app.get('/products' , async(req , res) => {
             console.log(req.query)
             const email = req.query.email;
@@ -66,6 +127,7 @@ async function run() {
             res.send(result);
         })
         app.post('/products' , async(req , res) => {
+            console.log("data blw blw blw" , req.headers);
             const newProduct = req.body;
             const result = await productsCollection.insertOne(newProduct);
             res.send(result);
@@ -99,14 +161,14 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/bids', async (req, res) => {
+        app.get('/bids', verifyFireBaseToken, async (req, res) => {
             const email = req.query.email;
-            const query = {};
-            if(email) {
-                query.buyer_email = email;
+            const query = {}
+            if (email) {
+                query.buyer_email = email
             }
-            const cursor = bidsCollection.find(query);
-            const result = await cursor.toArray();
+            const cursor = bidsCollection.find(query)
+            const result = await cursor.toArray()
             res.send(result);
         })
 
